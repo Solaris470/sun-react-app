8a346609a05da6ff1406c4e983dd5b12f1479aa6
Solaris470 <77920188+Solaris470@users.noreply.github.com>
2024-10-09 15:37:55 +0700

    edit form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", flowbite.content(),],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
 } satisfies Config;
